b7d3a27badcd818415a5eeaf98b9cf8eb351e0b6
SAAD <Chantal.Hajjar@usj.edu.lb>
2022-11-04 17:31:36 +0200

    const-dest-pushback-cout

--- main.cpp
+++ main.cpp
@@ -29,6 +29,61 @@ class MyVector{
 const MyVector operator+(MyVector v1, const MyVector& v2);
 
 // implementation
+
+MyVector::MyVector(int c)
+:capacity(c), size(0), ptr(new type[capacity])
+{
+  for (int i = 0; i < c ;++i){
+    ptr[i] = 0;
+  }
+} 
+MyVector::MyVector(type* p, int n)
+: capacity(n), size(n), ptr( new type [capacity])
+{
+
+//    for (int i = 0; i < size; ++i){
+//     ptr[i] = p[i];
+//    }
+   memcpy(ptr,p,size*sizeof(type));
+}
+
+MyVector::~MyVector(){
+    delete[] ptr;
+    ptr = nullptr;
+}
+ostream& operator<< (ostream& os, const MyVector& v){
+  os << "{" ;
+  int i;
+  for (i = 0; i < v.size-1; ++i){
+    os << v.ptr[i] << ",";
+  }
+  os << v.ptr[i] << "}";
+  return os;
+}
+void MyVector::push_back(type a){
+    if (size == capacity){
+        type* old = ptr;
+        ptr = new type [capacity*2];
+        memcpy(ptr,old,size*sizeof(type));
+        delete[] old;
+        capacity *= 2;
+        for (int i=size; i < capacity; ++i){
+            ptr[i] = 0;
+        }
+    }
+    ptr[size] = a;
+    size++;
+}
+//driver program
 int main(){
+    type tab [] = {1,2,3,4,5};
+    MyVector v1(tab,5);
+    MyVector v2;
+    v2.push_back(8);
+    v2.push_back(9);
+    cout << "v2 = " << v2 << endl;
+    cout << "v1 = " << v1 << endl;
+    v1.push_back(6);
+    cout << "v1=" << v1 << endl;
     return 0;
 }
